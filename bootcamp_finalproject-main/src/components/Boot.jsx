05b221e14c93b341bfa04156aee37b2d6e67012b
nikhila555 <88440179+nikhila555@users.noreply.github.com>
2021-08-05 14:08:54 +0530

    Add files via upload

---
+++ bootcamp_finalproject-main/src/components/Boot.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1>React Js Bootcamp</h1>
+      <p>This bootcamp is really a good start for Web Dev enthusisats.</p>
+    </div>
+  );
+}
+
+export default Boot;
